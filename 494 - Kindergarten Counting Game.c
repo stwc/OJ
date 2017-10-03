fb709caca8fad71402498019bc35985bcde3217c
shinobumw <g17596321@gmail.com>
2017-10-04 02:36:38 +0800

    Update

---
+++ 494 - Kindergarten Counting Game.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    char line[1001];
+    
+    while (gets(line)) {
+        int i, count = 0, isword = 0;
+
+        for (i = 0; line[i]; i++) {
+            if ((line[i] >= 'a' && line[i] <= 'z') || (line[i] >= 'A' && line[i] <= 'Z'))
+                isword = 1;
+            else {
+                count += isword;
+                isword = 0;
+            }
+        }
+        printf("%d\n", count);
+    }
+
+    return 0;    
+}
