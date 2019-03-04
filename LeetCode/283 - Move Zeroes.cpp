0937829a50ee8276ddca1623ebc9bbd5b0ea8238
shinobumw <shinobumw@users.noreply.github.com>
2019-03-05 01:19:41 +0800

    LeetCode 283 Move Zeroes

---
+++ LeetCode/283 - Move Zeroes.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    void moveZeroes(vector<int> &nums) {
+        for (int last = 0, cur = 0; cur < nums.size(); ++cur)
+            if (nums[cur] != 0)
+                swap(nums[cur], nums[last++]);
+    }
+};
