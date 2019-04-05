93ae123d104533dcf8e6cf1ab690d0d2d3742517
shinobumw <shinobumw@users.noreply.github.com>
2019-04-06 01:34:51 +0800

    LeetCode 589 590 tree

---
+++ LeetCode/590 - N-ary Tree Postorder Traversal.cpp
@@ -0,0 +1,35 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution
+{
+public:
+    vector<int> postorder(Node *root)
+    {
+        vector<int> res;
+        dfs(res, root);
+        return res;
+    }
+
+    void dfs(vector<int> &res, Node *root)
+    {
+        if (!root)
+            return;
+
+        for (auto &it : root->children)
+            dfs(res, it);
+        res.push_back(root->val);
+    }
+};
