1098f8a8c19b69627834546c646b89aab039b2b3
shinobumw <shinobumw@users.noreply.github.com>
2019-09-20 15:30:45 +0800

    Merge solutions

--- LeetCode/1 - Two Sum.cpp
+++ LeetCode/1 - Two Sum.cpp
@@ -1,6 +1,9 @@
-class Solution {
+// Ituitive but inefficient
+class Solution1
+{
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
         vector<int> sol;
         for (size_t i = 0; i < nums.size() - 1; ++i) {
             for (size_t j = i + 1; j < nums.size(); ++j) {
@@ -10,6 +13,25 @@ public:
                     return sol;
                 }
             }
+        }
+    }
+};
+
+// Better
+class Solution2
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        map<int, int> m;
+        vector<int> ret;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (m.count(target - nums[i])) {
+                ret.push_back(m[target - nums[i]]);
+                ret.push_back(i);
+                return ret;
+            }
+            m[nums[i]] = i;
         }
     }
 };
--- LeetCode/442 - Find All Duplicates in an Array.cpp
+++ LeetCode/442 - Find All Duplicates in an Array.cpp
@@ -1,4 +1,4 @@
-class Solution
+class Solution1
 {
 public:
     vector<int> findDuplicates(vector<int> &nums)
@@ -13,6 +13,21 @@ public:
         for (int i = 0; i < nums.size(); ++i) {
             if (nums[i] != i + 1)
                 ret.push_back(nums[i]);
+        }
+        return ret;
+    }
+};
+
+class Solution2
+{
+public:
+    vector<int> findDuplicates(vector<int> &nums)
+    {
+        vector<int> ret;
+        for (int i = 0; i < nums.size(); ++i) {
+            nums[abs(nums[i]) - 1] *= -1;
+            if (nums[abs(nums[i]) - 1] > 0)
+                ret.push_back(abs(nums[i]));
         }
         return ret;
     }
